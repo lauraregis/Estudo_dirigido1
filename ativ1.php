826fe6d611302a72762503c39c0c49c9b104ae25
Laurinha <48133541+lauraregis@users.noreply.github.com>
2019-03-14 20:24:22 -0300

    Add files via upload

---
+++ ativ1.php
@@ -0,0 +1,5 @@
+<? php
+print "Digite a quantidade de metros para converter:";
+$metros  = (float) fgets (STDIN);
+$milimetros  =  $metros * 1000;
+print  "\ n$metros o resultado da conversão é : $milimetros milimetros \n\n";
