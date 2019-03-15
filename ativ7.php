67d0ddaa534aaefa306cdbd1e65946de1228dea7
Laurinha <48133541+lauraregis@users.noreply.github.com>
2019-03-15 14:08:58 -0300

    Add files via upload

---
+++ ativ7.php
@@ -0,0 +1,9 @@
+<?php
+print "Quantos km você percorreu com o carro?: ";
+$km = (int) fgets (STDIN);
+print "Quantos dias você ficou com o carro?: ";
+$dias = (int) fgets(STDIN);
+$precokm = $km * 0.15;
+$precodias = $dias * 60;
+$precofinal = round($precokm + $precodias);
+print "\n Você ira pagar: $precofinal ";
